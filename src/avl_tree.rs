28bda55eb1d031c172f72958e5e689cfa9cf55cf
abin <1655853551@qq.com>
2023-01-22 19:44:02 +0800

    feat: avl_tree delete

--- src/avl_tree.rs
+++ src/avl_tree.rs
@@ -10,6 +10,7 @@ enum Side {
   Right,
 }
 
+// 取非逻辑
 impl Not for Side {
   type Output = Side;
   fn not(self) -> Self::Output {
@@ -29,6 +30,7 @@ struct AVLNode<T: Ord> {
 }
 
 impl<T: Ord> AVLNode<T> {
+  // 平衡当前节点子树
   fn rebalance(&mut self) {
     self.update_height();
     let side = match self.balance_factor() {
@@ -42,11 +44,11 @@ impl<T: Ord> AVLNode<T> {
     }
     self.rotate(!side);
   }
-
+  // 更新节点高度
   fn update_height(&mut self) {
     self.height = 1 + max(height(&self.left), height(&self.right))
   }
-
+  // 平衡因子计算
   fn balance_factor(&self) -> i8 {
     let left = height(&self.left);
     let right = height(&self.right);
@@ -56,14 +58,14 @@ impl<T: Ord> AVLNode<T> {
       -((left - right) as i8)
     }
   }
-
+  // 获取子树可变引用
   fn child(&mut self, side: Side) -> &mut Option<Box<AVLNode<T>>> {
     match side {
       Side::Left => &mut self.left,
       Side::Right => &mut self.right,
     }
   }
-
+  // 旋转操作
   fn rotate(&mut self, side: Side) {
     let mut subtree = self.child(!side).take().unwrap();
     *self.child(!side) = subtree.child(side).take();
@@ -73,7 +75,7 @@ impl<T: Ord> AVLNode<T> {
     self.update_height();
   }
 }
-
+// 高度计算
 fn height<T: Ord>(x: &Option<Box<AVLNode<T>>>) -> usize {
   match x {
     None => 0,
@@ -102,6 +104,14 @@ impl<T: Ord> AVLTree<T> {
     }
     inserted
   }
+
+  pub fn remove(&mut self, val: T) -> bool {
+    let removed = remove(&mut self.root, val);
+    if removed {
+      self.length -= 1
+    }
+    removed
+  }
 }
 
 fn insert<T: Ord>(tree: &mut Option<Box<AVLNode<T>>>, val: T) -> bool {
@@ -111,11 +121,13 @@ fn insert<T: Ord>(tree: &mut Option<Box<AVLNode<T>>>, val: T) -> bool {
       Ordering::Less => insert(&mut node.left, val),
       Ordering::Greater => insert(&mut node.right, val),
     };
+    // 如果插入新节点，需要重新平衡
     if inserted {
       node.rebalance();
     }
     inserted
   } else {
+    // 如果树为空，直接插入
     *tree = Some(Box::new(AVLNode {
       val,
       height: 1,
@@ -126,11 +138,75 @@ fn insert<T: Ord>(tree: &mut Option<Box<AVLNode<T>>>, val: T) -> bool {
   }
 }
 
+fn remove<T: Ord>(tree: &mut Option<Box<AVLNode<T>>>, val: T) -> bool {
+  if let Some(node) = tree {
+    let removed = match val.cmp(&node.val) {
+      Ordering::Less => remove(&mut node.left, val),
+      Ordering::Greater => remove(&mut node.right, val),
+      Ordering::Equal => {
+        *tree = match (node.left.take(), node.right.take()) {
+          // 叶子节点直接删除
+          (None, None) => None,
+          // 只有一个子树，直接替换
+          (Some(left), None) => Some(left),
+          (None, Some(right)) => Some(right),
+          // 有两个子树，找到右子树最小节点替换
+          (Some(left), Some(right)) => Some(merge(left, right)),
+        };
+        return true;
+      }
+    };
+    if removed {
+      node.rebalance();
+    }
+    removed
+  } else {
+    // 如果树为空，直接返回false
+    false
+  }
+}
+
+/// 合并两个子树
+fn merge<T: Ord>(left: Box<AVLNode<T>>, right: Box<AVLNode<T>>) -> Box<AVLNode<T>> {
+  let mut op_right = Some(right);
+  // 合并后的子树根节点为右子树最小节点
+  let mut root = take_min(&mut op_right).unwrap();
+  // 左子树为原左子树，右子树为删除最小节点后平衡的右子树
+  root.left = Some(left);
+  root.right = op_right;
+  // 重新平衡
+  root.rebalance();
+  root
+}
+
+/// 取出最小节点
+fn take_min<T: Ord>(tree: &mut Option<Box<AVLNode<T>>>) -> Option<Box<AVLNode<T>>> {
+  if let Some(mut node) = tree.take() {
+    if let Some(small) = take_min(&mut node.left) {
+      // 回溯时，需要重新平衡
+      node.rebalance();
+      // 各个根节点重新指向平衡后的子树
+      *tree = Some(node);
+      // 逐级向上传最小节点
+      Some(small)
+    } else {
+      // 用最小节点的右子树取代该节点位置，如果没有右子树，则为空
+      *tree = node.right.take();
+      // 返回该最小节点
+      Some(node)
+    }
+  } else {
+    None
+  }
+}
+
 #[cfg(test)]
 mod tests {
-  use super::*;
+  use super::AVLTree;
+
   #[test]
   fn test_avl_tree() {
+    // test insert
     let mut avl_tree = AVLTree::new();
     avl_tree.insert(32);
     avl_tree.insert(14);
@@ -141,7 +217,18 @@ mod tests {
     avl_tree.insert(23);
     avl_tree.insert(31);
     avl_tree.insert(30);
+    if let Some(root) = &avl_tree.root {
+      if let Some(right) = &root.right {
+        if let Some(left) = &right.left {
+          assert_eq!(left.val, 30);
+        }
+      }
+    };
 
-    println!("{:?}", avl_tree);
+    println!("{:?}", &avl_tree.root);
+
+    // test remove
+    avl_tree.remove(32);
+    println!("{:?}", &avl_tree.root);
   }
 }
